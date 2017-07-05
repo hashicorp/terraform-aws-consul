2615e9a76fce0c852cf7897296c0388305157168
Josh Padnick <josh.padnick@gmail.com>
2017-07-05 13:51:31 -0700

    Fix typo.

--- _ci/publish-amis.sh
+++ _ci/publish-amis.sh
@@ -41,7 +41,7 @@ publish-ami \
   --source-ami-region "$PACKER_TEMPLATE_DEFAULT_REGION" \
   --output-markdown > "$AMI_LIST_MARKDOWN_DIR/$PACKER_BUILD_NAME-list.md" \
   --markdown-title-text "$PACKER_BUILD_NAME: Latest Public AMIs" \
-  --markdown-description-text "**WARNING! Do NOT use these AMIs in a production setting. They are meant only to make
+  --markdown-description-text "**WARNING! Do NOT use these AMIs in a production setting.** They are meant only to make
     initial experiments with this blueprint more convenient."
 
 # Git add, commit, and push the newly created AMI IDs as a markdown doc to the repo
