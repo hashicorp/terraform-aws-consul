00494c250ab82926950a0f618daaeeedd5c2e3f1
patoarvizu <pato@asapp.com>
2018-04-09 16:53:01 -0400

    Add template variable logic in the user data script templates

--- examples/example-with-encryption/user-data-client.sh
+++ examples/example-with-encryption/user-data-client.sh
@@ -10,6 +10,15 @@ set -e
 exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 
 # These variables are passed in via Terraform template interplation
-/opt/consul/bin/run-consul --client --cluster-tag-key "${cluster_tag_key}" --cluster-tag-value "${cluster_tag_value}" ${gossip_encryption_configuration} ${rpc_encryption_configuration}
+if [[ "${enable_gossip_encryption}" == "true" && ! -z "${gossip_encryption_key}" ]]; then
+  # We're setting the encryption key in plain text here to simplify the example, but in production you should inject it more securely, e.g. retrieving it from KMS.
+  gossip_encryption_configuration="--enable-gossip-encryption --gossip-encryption-key ${gossip_encryption_key}"
+fi
+
+if [[ "${enable_rpc_encryption}" == "true" && ! -z "${ca_path}" && ! -z "${cert_file_path}" && ! -z "${key_file_path}" ]]; then
+  rpc_encryption_configuration="--enable-rpc-encryption --ca-path ${ca_path} --cert-file-path ${cert_file_path} --key-file-path ${key_file_path}"
+fi
+
+/opt/consul/bin/run-consul --client --cluster-tag-key "${cluster_tag_key}" --cluster-tag-value "${cluster_tag_value}" $gossip_encryption_configuration $rpc_encryption_configuration
 
 # You could add commands to boot your other apps here
