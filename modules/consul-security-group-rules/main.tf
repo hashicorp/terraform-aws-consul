dd60325b26ae59c4ccc6ac3fe2f5e2d50f063dbd
Bruno Mattarollo <bruno.mattarollo@gmail.com>
2018-04-12 09:59:45 +1000

    DRY up security group rules
    Include the `consul-client-security-group-rules` to remove the duplication of serf_lan_port rules.

--- modules/consul-security-group-rules/main.tf
+++ modules/consul-security-group-rules/main.tf
@@ -24,28 +24,6 @@ resource "aws_security_group_rule" "allow_cli_rpc_inbound" {
   security_group_id = "${var.security_group_id}"
 }
 
-resource "aws_security_group_rule" "allow_serf_lan_tcp_inbound" {
-  count       = "${length(var.allowed_inbound_cidr_blocks) >= 1 ? 1 : 0}"
-  type        = "ingress"
-  from_port   = "${var.serf_lan_port}"
-  to_port     = "${var.serf_lan_port}"
-  protocol    = "tcp"
-  cidr_blocks = ["${var.allowed_inbound_cidr_blocks}"]
-
-  security_group_id = "${var.security_group_id}"
-}
-
-resource "aws_security_group_rule" "allow_serf_lan_udp_inbound" {
-  count       = "${length(var.allowed_inbound_cidr_blocks) >= 1 ? 1 : 0}"
-  type        = "ingress"
-  from_port   = "${var.serf_lan_port}"
-  to_port     = "${var.serf_lan_port}"
-  protocol    = "udp"
-  cidr_blocks = ["${var.allowed_inbound_cidr_blocks}"]
-
-  security_group_id = "${var.security_group_id}"
-}
-
 resource "aws_security_group_rule" "allow_serf_wan_tcp_inbound" {
   count       = "${length(var.allowed_inbound_cidr_blocks) >= 1 ? 1 : 0}"
   type        = "ingress"
@@ -123,28 +101,6 @@ resource "aws_security_group_rule" "allow_cli_rpc_inbound_from_security_group_id
   security_group_id = "${var.security_group_id}"
 }
 
-resource "aws_security_group_rule" "allow_serf_lan_tcp_inbound_from_security_group_ids" {
-  count                    = "${length(var.allowed_inbound_security_group_ids)}"
-  type                     = "ingress"
-  from_port                = "${var.serf_lan_port}"
-  to_port                  = "${var.serf_lan_port}"
-  protocol                 = "tcp"
-  source_security_group_id = "${element(var.allowed_inbound_security_group_ids, count.index)}"
-
-  security_group_id = "${var.security_group_id}"
-}
-
-resource "aws_security_group_rule" "allow_serf_lan_udp_inbound_from_security_group_ids" {
-  count                    = "${length(var.allowed_inbound_security_group_ids)}"
-  type                     = "ingress"
-  from_port                = "${var.serf_lan_port}"
-  to_port                  = "${var.serf_lan_port}"
-  protocol                 = "udp"
-  source_security_group_id = "${element(var.allowed_inbound_security_group_ids, count.index)}"
-
-  security_group_id = "${var.security_group_id}"
-}
-
 resource "aws_security_group_rule" "allow_serf_wan_tcp_inbound_from_security_group_ids" {
   count                    = "${length(var.allowed_inbound_security_group_ids)}"
   type                     = "ingress"
@@ -198,4 +154,18 @@ resource "aws_security_group_rule" "allow_dns_udp_inbound_from_security_group_id
   source_security_group_id = "${element(var.allowed_inbound_security_group_ids, count.index)}"
 
   security_group_id = "${var.security_group_id}"
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# THE CONSUL-CLIENT SPECIFIC INBOUND/OUTBOUND RULES COME FROM THE CONSUL-CLIENT-SECURITY-GROUP-RULES MODULE
+# ---------------------------------------------------------------------------------------------------------------------
+
+module "client_security_group_rules" {
+  source = "../consul-client-security-group-rules"
+
+  security_group_id                  = "${var.security_group_id}"
+  allowed_inbound_cidr_blocks        = ["${var.allowed_inbound_cidr_blocks}"]
+  allowed_inbound_security_group_ids = ["${var.allowed_inbound_security_group_ids}"]
+
+  serf_lan_port   = "${var.serf_lan_port}"
 }
