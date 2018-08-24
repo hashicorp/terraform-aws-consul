c1cf12c37ccb6363f066f7528e5f32ea6370c2a8
Yevgeniy Brikman <brikis98@gmail.com>
2018-08-24 12:54:43 +0100

    Fix script params

--- _ci/publish-amis.sh
+++ _ci/publish-amis.sh
@@ -7,28 +7,21 @@
 
 set -e
 
-readonly PACKER_TEMPLATE_PATH="/home/ubuntu/$CIRCLE_PROJECT_REPONAME/examples/consul-ami/consul.json"
-readonly PACKER_TEMPLATE_DEFAULT_REGION="us-east-1"
-
-# In CircleCI, every build populates the branch name in CIRCLE_BRANCH except builds triggered by a new tag, for which
-# the CIRCLE_BRANCH env var is empty. We assume tags are only issued against the master branch.
-readonly BRANCH_NAME="${CIRCLE_BRANCH:-master}"
-
-readonly PACKER_BUILD_NAME="$1"
-
-if [[ -z "$PACKER_BUILD_NAME" ]]; then
-  echo "ERROR: You must pass in the Packer build name as the first argument to this function."
+if [[ "$#" -ne 2 ]]; then
+  echo "Usage: publish-amis.sh PACKER_TEMPLATE_PATH BUILDER_NAME"
   exit 1
 fi
 
-echo "Checking out branch $BRANCH_NAME to make sure we do all work in a branch and not in detached HEAD state"
-git checkout "$BRANCH_NAME"
+readonly packer_template_path="$1"
+readonly builder_name="$2"
 
-regions_response=$(aws ec2 describe-regions --region "$PACKER_TEMPLATE_DEFAULT_REGION")
+regions_response=$(aws ec2 describe-regions --region "us-east-1")
 all_aws_regions=$(echo "$regions_response" | jq -r '.Regions | map(.RegionName) | join(",")')
 
+echo "Building Packer template $packer_template_path (builder: $builder_name) and sharing it with all AWS accounts in the following regions: $all_aws_regions"
+
 packer build \
-  --only="$PACKER_BUILD_NAME" \
+  --only="$builder_name" \
   -var copy_ami_to_regions="$all_aws_regions" \
   -var share_ami_with_groups="all" \
-  "$PACKER_TEMPLATE_PATH"
+  "$packer_template_path"
