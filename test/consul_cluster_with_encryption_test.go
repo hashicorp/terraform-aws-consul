46007fc0b016875b1413689278c151444b41a94a
Etiene Dalcol <dalcol@etiene.net>
2018-08-28 19:07:41 +0100

    Add test for consul enterprise installation

--- test/consul_cluster_with_encryption_test.go
+++ test/consul_cluster_with_encryption_test.go
@@ -4,12 +4,10 @@ import "testing"
 
 func TestConsulClusterWithEncryptionUbuntuAmi(t *testing.T) {
 	t.Parallel()
-	runConsulClusterTest(t, "ubuntu16-ami", "examples/example-with-encryption", "../examples/example-with-encryption/packer/consul-with-certs.json")
+	runConsulClusterTest(t, "ubuntu16-ami", "examples/example-with-encryption", "../examples/example-with-encryption/packer/consul-with-certs.json", "ubuntu", "")
 }
 
 func TestConsulClusterWithEncryptionAmazonLinuxAmi(t *testing.T) {
 	t.Parallel()
-	runConsulClusterTest(t, "amazon-linux-ami", "examples/example-with-encryption", "../examples/example-with-encryption/packer/consul-with-certs.json")
+	runConsulClusterTest(t, "amazon-linux-ami", "examples/example-with-encryption", "../examples/example-with-encryption/packer/consul-with-certs.json", "ec2-user", "")
 }
-
-
---
+++ test/consul_enterprise_test.go
@@ -0,0 +1,17 @@
+package test
+
+import (
+	"os"
+	"testing"
+)
+
+// Test the example in the root folder
+func TestConsulInstallFromURLWithUbuntuAmi(t *testing.T) {
+	t.Parallel()
+	runConsulClusterTest(t, "ubuntu16-ami", ".", "../examples/consul-ami/consul.json", "ubuntu", os.Getenv("CONSUL_DOWLOAD_URL"))
+}
+
+func TestConsulInstallFromURLWithAmazonLinuxAmi(t *testing.T) {
+	t.Parallel()
+	runConsulClusterTest(t, "amazon-linux-ami", ".", "../examples/consul-ami/consul.json", "ec2-user", os.Getenv("CONSUL_DOWLOAD_URL"))
+}
