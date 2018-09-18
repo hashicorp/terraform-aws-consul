3c7677caceb57bbf0738c633febcaca730cbd776
James Johnson <James.Johnson@equifax.com>
2018-09-18 10:25:02 -0400

    Add the consul cluster's security group to the allowed inbound security group ids so that our nodes can communicate

--- modules/consul-cluster/main.tf
+++ modules/consul-cluster/main.tf
@@ -140,8 +140,8 @@ module "security_group_rules" {
 
   security_group_id                    = "${aws_security_group.lc_security_group.id}"
   allowed_inbound_cidr_blocks          = ["${var.allowed_inbound_cidr_blocks}"]
-  allowed_inbound_security_group_ids   = ["${var.allowed_inbound_security_group_ids}"]
-  allowed_inbound_security_group_count = "${var.allowed_inbound_security_group_count}"
+  allowed_inbound_security_group_ids   = "${concat(list(aws_security_group.lc_security_group.id), var.allowed_inbound_security_group_ids)}"
+  allowed_inbound_security_group_count = "${1+var.allowed_inbound_security_group_count}"
 
   server_rpc_port = "${var.server_rpc_port}"
   cli_rpc_port    = "${var.cli_rpc_port}"
