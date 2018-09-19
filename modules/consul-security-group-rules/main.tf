00454de483465272d5d0af9ff3e2808234a3cbdd
James Johnson <James.Johnson@equifax.com>
2018-09-19 08:44:38 -0400

    Bah... not enough coffee yet

--- modules/consul-security-group-rules/main.tf
+++ modules/consul-security-group-rules/main.tf
@@ -156,6 +156,78 @@ resource "aws_security_group_rule" "allow_dns_udp_inbound_from_security_group_id
   security_group_id = "${var.security_group_id}"
 }
 
+# Similar to the *_inbound_from_security_group_ids rules, allow inbout from ourself
+
+resource "aws_security_group_rule" "allow_server_rpc_inbound_from_self" {
+  type      = "ingress"
+  from_port = "${var.server_rpc_port}"
+  to_port   = "${var.server_rpc_port}"
+  protocol  = "tcp"
+  self      = true
+
+  security_group_id = "${var.security_group_id}"
+}
+
+resource "aws_security_group_rule" "allow_cli_rpc_inbound_from_self" {
+  type      = "ingress"
+  from_port = "${var.cli_rpc_port}"
+  to_port   = "${var.cli_rpc_port}"
+  protocol  = "tcp"
+  self      = true
+
+  security_group_id = "${var.security_group_id}"
+}
+
+resource "aws_security_group_rule" "allow_serf_wan_tcp_inbound_from_self" {
+  type      = "ingress"
+  from_port = "${var.serf_wan_port}"
+  to_port   = "${var.serf_wan_port}"
+  protocol  = "tcp"
+  self      = true
+
+  security_group_id = "${var.security_group_id}"
+}
+
+resource "aws_security_group_rule" "allow_serf_wan_udp_inbound_from_self" {
+  type      = "ingress"
+  from_port = "${var.serf_wan_port}"
+  to_port   = "${var.serf_wan_port}"
+  protocol  = "udp"
+  self      = true
+
+  security_group_id = "${var.security_group_id}"
+}
+
+resource "aws_security_group_rule" "allow_http_api_inbound_from_self" {
+  type      = "ingress"
+  from_port = "${var.http_api_port}"
+  to_port   = "${var.http_api_port}"
+  protocol  = "tcp"
+  self      = true
+
+  security_group_id = "${var.security_group_id}"
+}
+
+resource "aws_security_group_rule" "allow_dns_tcp_inbound_from_self" {
+  type      = "ingress"
+  from_port = "${var.dns_port}"
+  to_port   = "${var.dns_port}"
+  protocol  = "tcp"
+  self      = true
+
+  security_group_id = "${var.security_group_id}"
+}
+
+resource "aws_security_group_rule" "allow_dns_udp_inbound_from_self" {
+  type      = "ingress"
+  from_port = "${var.dns_port}"
+  to_port   = "${var.dns_port}"
+  protocol  = "udp"
+  self      = true
+
+  security_group_id = "${var.security_group_id}"
+}
+
 # ---------------------------------------------------------------------------------------------------------------------
 # THE CONSUL-CLIENT SPECIFIC INBOUND/OUTBOUND RULES COME FROM THE CONSUL-CLIENT-SECURITY-GROUP-RULES MODULE
 # ---------------------------------------------------------------------------------------------------------------------
