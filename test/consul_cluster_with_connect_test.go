aee822c0d2302acbf81c848c272d2a567bc13d7c
Nirmal Thacker <nirmalthacker@gmail.com>
2020-05-14 16:28:18 -0700

    addressing some comments in PR

--- test/consul_cluster_with_connect_test.go
+++ test/consul_cluster_with_connect_test.go
@@ -4,12 +4,11 @@ import (
 	"testing"
 )
 
-/*
 // Test the example in the example-with-consul-connect folder
 func TestConsulClusterWithUbuntu16Ami(t *testing.T) {
 	t.Parallel()
 	runConsulConnectTest(t, "ubuntu16-ami", "examples/example-with-consul-connect", "../examples/consul-ami/consul.json", "ubuntu")
-}*/
+}
 
 // Test the example in the example-with-consul-connect folder
 func TestConsulConnectWithUbuntu18Ami(t *testing.T) {
@@ -17,10 +16,9 @@ func TestConsulConnectWithUbuntu18Ami(t *testing.T) {
 	runConsulConnectTest(t, "ubuntu18-ami", "examples/example-with-consul-connect", "../examples/consul-ami/consul.json", "ubuntu")
 }
 
-/*
 // Test the example in the example-with-consul-connect folder
 func TestConsulClusterWithAmazonLinuxAmi(t *testing.T) {
 	t.Parallel()
 	runConsulConnectTest(t, "amazon-linux-2-ami", "examples/example-with-consul-connect", "../examples/consul-ami/consul.json", "ec2-user")
 }
-*/
+
