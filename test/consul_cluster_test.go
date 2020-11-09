2167256b9c90f2f9e6ba4c69f6cff7b0a6238a38
Mirko Pluhar <m.pluhar+github@gmail.com>
2020-11-10 12:41:09 +1300

    Ubuntu 20.04 Support

--- test/consul_cluster_test.go
+++ test/consul_cluster_test.go
@@ -17,6 +17,11 @@ func TestConsulClusterWithUbuntu18Ami(t *testing.T) {
 }
 
 // Test the example in the root folder
+func TestConsulClusterWithUbuntu20Ami(t *testing.T) {
+	t.Parallel()
+	runConsulClusterTest(t, "ubuntu20-ami", ".", "../examples/consul-ami/consul.json", "ubuntu", "")
+}
+// Test the example in the root folder
 func TestConsulClusterWithAmazonLinuxAmi(t *testing.T) {
 	t.Parallel()
 	runConsulClusterTest(t, "amazon-linux-2-ami", ".", "../examples/consul-ami/consul.json", "ec2-user", "")
