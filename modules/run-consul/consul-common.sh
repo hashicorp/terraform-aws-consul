d11727eabfbaecaf8991611a01601475e917a7a1
Josh Feierman <josh@sqljosh.com>
2021-04-19 10:48:21 -0400

    Functions to read and write ACL tokens have been added to the script to run Consul, as well as a function to calculate the name of the SSM parameter to store the bootstrap token in.

---
+++ modules/run-consul/consul-common.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+source "/opt/gruntwork/bash-commons/log.sh"
+source "/opt/gruntwork/bash-commons/string.sh"
+source "/opt/gruntwork/bash-commons/assert.sh"
+source "/opt/gruntwork/bash-commons/aws-wrapper.sh"
+
+function bootstrap_acl {
+  
+  
+  local token
+  
+}
