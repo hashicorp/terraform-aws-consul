7ee82d1e2b73cdbbe1655248fe2cfd8eb7700b05
dimisjim <dimitris.moraitidis@gmail.com>
2021-11-24 10:32:49 +0100

    minor var description fix

--- variables.tf
+++ variables.tf
@@ -67,7 +67,7 @@ variable "enable_https_port" {
 }
 
 variable "subnet_ids" {
-  description = "The subnet IDs into which the EC2 Instances should be deployed. We recommend one subnet ID per node in the cluster_size variable. At least one of var.subnet_ids or var.availability_zones must be non-empty."
+  description = "The subnet IDs into which the EC2 Instances should be deployed. We recommend one subnet ID per node in the cluster_size variable."
   type        = list(string)
   default     = null
 }
